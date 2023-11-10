c60aadf60dbb9964b2c0810bead3f2b4e2a05f2a
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-10 13:49:54 +0600

    Add SideBar component and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SideBar from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex">
+          <SideBar />
+          {/* Client Provider Notifications */}
+          <div className='bg-[#343541] flex-1'>
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
